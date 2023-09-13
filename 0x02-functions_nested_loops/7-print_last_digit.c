509d9e0ee1f820757bc8a0bf13c2b3d138de8c2a
B-Letlalo <letlaloboikano@gmail.com>
2023-09-13 13:13:18 -0700

    7-print_last_digit.c_II

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,6 +6,19 @@
  */
 int print_last_digit(int i)
 {
-	i  = i % 10;
-	return	(i);
+	int last_digit;
+
+	last_digit = i % 10;
+	/*if last digit is a negative number, turns it into a positive number*/
+	if (last_digit < 0)
+	{
+		last_digit = last_digit * -1;
+	}
+	/*if last digit is positive, leave as is*/
+	else
+	{
+		last_digit = last_digit;
+	}
+	_putchar(last_digit + '0');
+	return	(last_digit);
 }
