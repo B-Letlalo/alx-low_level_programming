354e57db5046dda49d407308a1bb625dd333c75d
B-Letlalo <letlaloboikano@gmail.com>
2023-09-14 04:47:06 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ * @c: function parameter
+ * Return: 0 if lowercase and 1 if uppercase
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return	(1);
+	}
+	else if (c >= 'a' && c <= 'z')
+	{
+		return	(0);
+	}
+	else
+	{
+		return	(-1);
+	}
+}
