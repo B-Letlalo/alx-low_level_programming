15725808f7514dd843c3d64588791fec08eb86db
B-Letlalo <letlaloboikano@gmail.com>
2023-09-18 14:17:31 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: function argument, char
+ */
+void _puts(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		_putchar(str);
+	}
+}
