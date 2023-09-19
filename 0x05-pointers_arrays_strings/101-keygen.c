d1cea417727c7f02bf5da6a39c83af0ba15f0a62
B-Letlalo <letlaloboikano@gmail.com>
2023-09-19 07:03:59 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int sum;
+	char c;
+
+	srand(time(NULL));
+	sum = 0;
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		_putchar(c);
+	}
+	_putchar(2772 - sum);
+
+	return	(0);
+}
