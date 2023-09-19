fbef7639817bb05b335d046180610a9925348143
B-Letlalo <letlaloboikano@gmail.com>
2023-09-18 17:36:52 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,4 +4,16 @@
  * starting with the first character, followed by a new line
  * @str: function parameter
  */
+void puts2(char *str)
+{
+	int n;
 
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		if (n % 2 == 0)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
