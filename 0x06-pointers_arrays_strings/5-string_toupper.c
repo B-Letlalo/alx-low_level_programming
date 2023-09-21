abd57b44da9822fe2ed42ccea626579a1c883cf9
B-Letlalo <letlaloboikano@gmail.com>
2023-09-21 15:36:09 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
- * @n: function parameter, char
- * Return: char in uppercase
+ * @n: function parameter
+ * Return: uppercase characters
  */
 char *string_toupper(char *n)
 {
@@ -14,10 +14,7 @@ char *string_toupper(char *n)
 		{
 			n[i] = n[i] - 32;
 			i++;
-			return	(n);
 		}
-		else
-		{
-			return	(0);
-		}
+	}
+	return	(n);
 }
