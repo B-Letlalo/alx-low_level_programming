9f7b4b3c4015cf62d45c2057fd2f3a9085889563
B-Letlalo <letlaloboikano@gmail.com>
2023-09-22 13:27:03 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,3 +7,19 @@
  * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+	char *i = haystack;
+	char *p = needle;
+
+	while (*i == *p && *p != '\0')
+	{
+	i++;
+	p++;
+	}
+	if (*p == '\0')
+	return (haystack);
+	}
+	return (0);
+}
