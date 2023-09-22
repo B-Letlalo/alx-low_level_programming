083d6b17c4dda65218a1d2d23ff4c34cd9be1a45
B-Letlalo <letlaloboikano@gmail.com>
2023-09-22 13:14:56 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,3 +7,24 @@
  * which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int _strspn(char *s, char *accept);
+	unsigned int i = 0;
+	int k;
+
+	while (*s)
+	{
+	for (k = 0; accept[k]; k++)
+	{
+	if (*s == accept[k])
+	{
+	i++;
+	break;
+	}
+	else if (accept[k + 1] == '\0')
+	return (i);
+	}
+	s++;
+	}
+	return (i);
+}
