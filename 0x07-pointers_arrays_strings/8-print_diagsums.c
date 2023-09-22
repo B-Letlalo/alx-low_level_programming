449feb8c645d336cfbf01b9e261b0451f6cf350a
B-Letlalo <letlaloboikano@gmail.com>
2023-09-22 13:23:21 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,3 +6,21 @@
  * @size: function parameter, size
  */
 void print_diagsums(int *a, int size)
+{
+	int count_1;
+	int count_2;
+	int i;
+
+	count_1 = 0;
+	count_2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+	count_1 = count_1 + a[i * size + i];
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+	count_2 += a[i * size + (size - i - 1)];
+	}
+	printf("%d, %d\n", count_1, count_2);
+}
