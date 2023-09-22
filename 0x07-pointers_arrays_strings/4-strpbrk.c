df9e7a2d125f4621c9c32bde45c9f572b1a0a8e4
B-Letlalo <letlaloboikano@gmail.com>
2023-09-22 13:17:48 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,3 +7,16 @@
  * or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+	if (s[i] == accept[j])
+	return (s + i);
+	}
+	}
+	return (0);
+}
