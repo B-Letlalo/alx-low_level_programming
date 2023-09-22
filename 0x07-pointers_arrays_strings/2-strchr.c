a5a37831abeb5e7fe59b8b65487d9404ffe25ee2
B-Letlalo <letlaloboikano@gmail.com>
2023-09-22 13:13:16 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,3 +7,13 @@
  * or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+	if (s[i] == c)
+	return (s + i);
+	}
+	return (0);
+}
