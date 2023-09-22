22460bafa364c58fab7bd03bcd0a7994839b9a5d
B-Letlalo <letlaloboikano@gmail.com>
2023-09-22 13:30:44 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,3 +4,16 @@
  * @a: function parameter, char
  */
 void print_chessboard(char (*a)[8])
+{
+	int k;
+	int r;
+
+	for (k = 0; k < 8; k++)
+	{
+		for (r = 0; r < 8; r++)
+		{
+			_putchar(a[k][r]);
+			_putchar('\n');
+		}
+	}
+}
