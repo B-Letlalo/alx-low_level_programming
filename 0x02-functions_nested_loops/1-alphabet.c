6e5e82dbaa499d43c8f91ca3e499e4c7b02d3105
B-Letlalo <letlaloboikano@gmail.com>
2023-09-26 03:22:49 -0700

    Alphabet print II

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
 void print_alphabet(void)
 {
 	/* declaring letters of the alphabet in a char array*/
-	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' ||
-		'k', 'l', 'm', 'n', 'o', 'o', 'p', 'q', 'r', 's', 't', 'u' ||
-			'v', 'w', 'x', 'y', 'z'};
+	char alphabet[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+		'w', 'x', 'y', 'z'};
 	int i = 0;
 
-	while (alphabet[i] < 26)
+	while (i < 26)
 	{
 		_putchar(alphabet[i]);
 		i++;
