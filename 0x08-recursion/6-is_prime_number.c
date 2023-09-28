eab45efcbddd1da682a222edd82b85fa88abec5e
B-Letlalo <letlaloboikano@gmail.com>
2023-09-28 14:30:34 -0700

    first commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0
+ * @n: integer parameter
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return	(0);
+	}
+}
