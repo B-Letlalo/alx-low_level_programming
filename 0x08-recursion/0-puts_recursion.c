62aa1aedecd11f847dcd400960925cd903fcd1e4
B-Letlalo <letlaloboikano@gmail.com>
2023-09-28 04:44:50 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: function parameter, string
+ */
+void _puts_recursion(char *s)
+{
+	/*Setting the counter*/
+	int i = 0;
+
+	/*Base case*/
+	if (s[i] != '\0')
+	{
+		_putchar('\n');
+	}
+	/* Recursive function*/
+	else
+	{
+		_putchar(s[i]);
+		_putchar(s[i + 1]);
+	}
+}
