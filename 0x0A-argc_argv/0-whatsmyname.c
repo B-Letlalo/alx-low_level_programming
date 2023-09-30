a61da662dd65a9ab70cf04252540d03828a785d3
B-Letlalo <letlaloboikano@gmail.com>
2023-09-30 09:01:47 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line
+ * Return: 0
+ * @argv: argument vector
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return	(0);
+}
