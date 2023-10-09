18755ee4f9be7ffb9fea1bb2cb4c04ee5db08dbf
B-Letlalo <letlaloboikano@gmail.com>
2023-10-09 02:01:05 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct my_dog - stores dog identification
+ * @name: name of the dog
+ * @owner: owner's name
+ * @age: dog's age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
