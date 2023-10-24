65bbd64d717fc70f5056aa48a9ba5e025e6e4306
B-Letlalo <letlaloboikano@gmail.com>
2023-10-24 06:38:42 -0700

    First task completed

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to first node
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int counter = 0;
+
+	if (h == NULL)
+	{
+		printf("Linked list is empty\n");
+		return	(0);
+	}
+	else
+	{
+		listint_t *ptr = (listint_t *)h;
+
+		while (ptr != NULL)
+		{
+			printf("%d\n", ptr->n);
+			counter++;
+			ptr = ptr->next;
+		}
+		return	(counter);
+	}
+}
