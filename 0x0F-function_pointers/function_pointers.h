ecea0b316eb68473bd01ec763f6ca5631e0a5ecc
B-Letlalo <letlaloboikano@gmail.com>
2023-10-24 13:24:18 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -7,4 +7,17 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int (*get_op_func(char *s))(int, int);
 int _putchar(char c);
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
+
 #endif
