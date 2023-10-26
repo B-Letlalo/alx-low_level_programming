65f2ab8466c4b9d1b47cbf4fb3d3303214ec19ba
B-Letlalo <letlaloboikano@gmail.com>
2023-10-26 15:59:44 -0700

    Little endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	unsigned char *byte = (unsigned char *)&num;
+
+	if (*byte == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
